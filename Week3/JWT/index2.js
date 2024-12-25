a8219268483481e41c0b5a27186f7a8e52af250e
Chirag <95089015+Chirag-04@users.noreply.github.com>
2024-12-25 13:26:21 +0530

    Update index2.js

--- Week3/JWT/index2.js
+++ Week3/JWT/index2.js
@@ -1,5 +1,5 @@
 //Write a function that takes in a username and password and returns a JWT token with the username encoded. Should return null if the username is not a valid email or if the password is less than 6 characters. Try using the zod library here
-
+// zod  = input validation library
 const jwt = require('jsonwebtoken');
 const jwtPassword = "secret";
 const zod  = require('zod');
